5b8786b7aa4413eb1329d98020604d660ee07fda
hotel-p <hotelmanagement1010@gmail.com>
2023-08-22 00:47:51 +0530

    Change grid item border property

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ let currentVar = 'X'
 cells.forEach((cell)=>{
     cell.addEventListener('click',()=>{
         cell.innerHTML = currentVar;
+        
         if(currentVar === 'X'){
+            cell.setAttribute('style', 'background-color:red; border: 4px solid white');
             currentVar = 'O';
         }
         else{
             currentVar = 'X';
+            cell.setAttribute('style', 'background-color:green; border: 4px solid white');
         }
     })
 })
