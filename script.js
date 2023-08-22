1bc9439dd1709e0a8296e4fef40fb716b830cd30
hotel-p <hotelmanagement1010@gmail.com>
2023-08-22 23:52:45 +0530

    Add winCheck function

--- script.js
+++ script.js
@@ -1,19 +1,98 @@
-const cells = document.querySelectorAll(".grid-item");
+const Player = (name,symbol)=>{
+    this.name = name;
+    this.symbol = symbol;
+    return {name,symbol}
+}
 
-let currentVar = 'X'
+let player1 = Player('Player1','X');
+let player2 = Player('Player2','O');
 
-cells.forEach((cell)=>{
-    cell.addEventListener('click',()=>{
-        cell.innerHTML = currentVar;
+const gameBoard = ()=>{
+    let gameArray = ['','','','','','','','',''];
+
+    const winCheck = (symbol,position)=>{
+        updateBoard(symbol,position);
+        const winningCombinations = [
+            [0, 1, 2], // Top row
+            [3, 4, 5], // Middle row
+            [6, 7, 8], // Bottom row
+            [0, 3, 6], // Left column
+            [1, 4, 7], // Middle column
+            [2, 5, 8], // Right column
+            [0, 4, 8], // Diagonal from top-left to bottom-right
+            [2, 4, 6]  // Diagonal from top-right to bottom-left
+        ];
         
-        if(currentVar === 'X'){
-            cell.setAttribute('style', 'background-color:red; border: 4px solid white');
-            currentVar = 'O';
-        }
-        else{
-            currentVar = 'X';
-            cell.setAttribute('style', 'background-color:green; border: 4px solid white');
-        }
-    })
-})
+        for (const combination of winningCombinations) {
+            console.log(combination[0]);
 
+            if(gameArray[combination[0]] == symbol && gameArray[combination[1]] == symbol && gameArray[combination[2]] == symbol){
+                console.log("wine return truw");
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    const updateBoard = (symbol,position)=>{
+        gameArray[position] = symbol; 
+    };
+
+    return { winCheck, gameArray };
+}
+
+const gameFlow = (()=>{
+    const winnerEle = document.querySelector('.winner');
+    const cells = document.querySelectorAll(".grid-item");
+    const reset = document.getElementById('reset');
+    let board = gameBoard();
+    let turn = player1;
+    let symbol = turn.symbol;
+    let position;
+    let gameOver = false;
+    
+    let gameState = ()=>{
+        if(turn == player1){
+            turn = player2;
+            symbol = turn.symbol;
+        }
+        else if(turn == player2){
+            turn = player1;
+            symbol = turn.symbol;
+        }
+    }
+
+    cells.forEach((cell)=>{
+        cell.addEventListener('click',()=>{
+            if(cell.innerHTML == '' && gameOver!=true){
+                cell.innerHTML = symbol;
+                position = cell.id;
+                if(turn == player1){
+                    cell.setAttribute('style', 'background-color:red; border: 4px solid white');
+                }
+                if(turn == player2){
+                    cell.setAttribute('style', 'background-color:green; border: 4px solid white');
+                }
+                if(board.winCheck(symbol,position)==true){
+                    gameOver = true;
+                    winner = turn.name;
+                    winnerEle.setAttribute('style', 'visibility: visible');
+                    winnerEle.innerHTML = `${winner} Wins`;
+                }
+                else{
+                    gameState();    
+                }
+            }
+        })
+    })
+
+    reset.addEventListener('click',()=>{
+        gameOver = false;
+        board.gameArray = ['','','','','','','','',''];
+        cells.forEach((cell)=>{
+            cell.innerHTML = '';
+            cell.style = '';
+        })
+    })
+
+})();
